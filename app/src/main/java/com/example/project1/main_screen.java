88841d0c537e4cb2d375a86921ec055a403974a3
yuri <jjli98@naver.com>
2021-09-08 22:20:48 +0900

    메인화면 구성(카드뷰), 로그인 시 user_name 불러오기

--- app/src/main/java/com/example/project1/main_screen.java
+++ app/src/main/java/com/example/project1/main_screen.java
@@ -1,14 +1,51 @@
 package com.example.project1;
 
 import android.os.Bundle;
+import android.transition.AutoTransition;
+import android.transition.TransitionManager;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 public class main_screen extends AppCompatActivity {
+
+    ImageButton arrow;
+    CardView cardView;
+    LinearLayout hiddenView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+
+        cardView = findViewById(R.id.base_cardview);
+        arrow = findViewById(R.id.arrow_button);
+        hiddenView = findViewById(R.id.hidden_view);
+
+        arrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(hiddenView.getVisibility() == View.VISIBLE){
+                    TransitionManager.beginDelayedTransition(cardView, new AutoTransition());
+                    hiddenView.setVisibility(View.GONE);
+                }
+                else{
+                    TransitionManager.beginDelayedTransition(cardView,
+                            new AutoTransition());
+                    hiddenView.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        String username = getIntent().getStringExtra("user_name");
+
+        final TextView user_name = (TextView) findViewById(R.id.user_name);
+        user_name.setText(username);
+
     }
 }
