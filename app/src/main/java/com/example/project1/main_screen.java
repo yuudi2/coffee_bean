071a94e83ce1dbc2c145d259b145260750bfac85
yuri <jjli98@naver.com>
2021-09-09 17:05:38 +0900

    카드뷰 재구성 , 슬라이드 메뉴 구현(슬라이드, 회원 이름 불러오기)

--- app/src/main/java/com/example/project1/main_screen.java
+++ app/src/main/java/com/example/project1/main_screen.java
@@ -3,19 +3,26 @@ package com.example.project1;
 import android.os.Bundle;
 import android.transition.AutoTransition;
 import android.transition.TransitionManager;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class main_screen extends AppCompatActivity {
 
-    ImageButton arrow;
-    CardView cardView;
-    LinearLayout hiddenView;
+    ImageButton arrow, arrow2, menu;
+    CardView cardView, cardView2;
+    LinearLayout hiddenView, hiddenView2;
+    RelativeLayout layout1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +33,13 @@ public class main_screen extends AppCompatActivity {
         cardView = findViewById(R.id.base_cardview);
         arrow = findViewById(R.id.arrow_button);
         hiddenView = findViewById(R.id.hidden_view);
+        cardView2 = findViewById(R.id.base_cardview2);
+        arrow2 = findViewById(R.id.arrow_button2);
+        hiddenView2 = findViewById(R.id.hidden_view2);
+        layout1 = findViewById(R.id.layout1);
+
+        menu = findViewById(R.id.menu_button);
+
 
         arrow.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,11 +47,30 @@ public class main_screen extends AppCompatActivity {
                 if(hiddenView.getVisibility() == View.VISIBLE){
                     TransitionManager.beginDelayedTransition(cardView, new AutoTransition());
                     hiddenView.setVisibility(View.GONE);
+                    arrow.setImageResource(R.drawable.ic_icon_down);
                 }
                 else{
                     TransitionManager.beginDelayedTransition(cardView,
                             new AutoTransition());
                     hiddenView.setVisibility(View.VISIBLE);
+                    arrow.setImageResource(R.drawable.ic_icon_up);
+                }
+            }
+        });
+
+        arrow2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(hiddenView2.getVisibility() == View.VISIBLE){
+                    TransitionManager.beginDelayedTransition(cardView2, new AutoTransition());
+                    hiddenView2.setVisibility(View.GONE);
+                    arrow2.setImageResource(R.drawable.ic_icon_down);
+                }
+                else{
+                    TransitionManager.beginDelayedTransition(cardView2,
+                            new AutoTransition());
+                    hiddenView2.setVisibility(View.VISIBLE);
+                    arrow2.setImageResource(R.drawable.ic_icon_up);
                 }
             }
         });
@@ -46,6 +79,37 @@ public class main_screen extends AppCompatActivity {
 
         final TextView user_name = (TextView) findViewById(R.id.user_name);
         user_name.setText(username);
+
+
+        //navigation header 회원 이름
+        LinearLayout ll_navigation_container = (LinearLayout) LayoutInflater.from(this).inflate(R.layout.navigation_header, null);
+
+        final TextView tv_username = new TextView(this);
+        tv_username.setTextColor(getResources().getColor(R.color.white));
+        tv_username.setTextSize(22);
+
+        tv_username.setText(username + "님.");
+
+        ll_navigation_container.addView(tv_username);
+
+
+
+
+
+       final DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+
+        findViewById(R.id.menu_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // start에 지정된 Drawer 열기
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+
+        NavigationView navigationView = findViewById(R.id.navigationView);
+        navigationView.addHeaderView(ll_navigation_container);
+        navigationView.setItemIconTintList(null);
 
     }
 }
