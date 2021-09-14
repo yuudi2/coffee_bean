8a480e9e7415c42f0eb47c9dc5bd6c6d065f24c9
yuri <jjli98@naver.com>
2021-09-14 16:09:08 +0900

    메뉴 뷰홀더 구현

--- app/src/main/java/com/example/project1/menu_choice.java
+++ app/src/main/java/com/example/project1/menu_choice.java
@@ -5,39 +5,54 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-//import Adapter.CoffeeViewAdaper;
-//import Data.CoffeeData;
+
+import java.util.ArrayList;
+
+import Adapter.CoffeeViewAdapter;
+import Data.CoffeeData;
 
 public class menu_choice extends AppCompatActivity {
 
-//    private RecyclerView mRecyclerView = null;
-//    private CoffeeViewAdaper mAdapter2 = null;
+    private RecyclerView recyclerView = null;
+    private CoffeeViewAdapter adapter = null;
 
-//    ArrayList<CoffeeData> coffeeList = new ArrayList<>();
+
+   ArrayList<CoffeeData> coffeeList = new ArrayList<>();
+
+    int [] coffee_img = {R.drawable.coffee1, R.drawable.coffee2, R.drawable.coffee3,
+            R.drawable.coffee4, R.drawable.coffee5, R.drawable.coffee6};
+    String [] coffee_name = {"헤이즐넛아메리카노IB","단팥IB","인절미IB","블랙다이몬 아이스커피","블랙다이몬 카페라떼(R)","블랙다이몬 카페수아(R)"};
+    int [] coffee_price = {6000,6900,6800,5800,6300,7000};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_choice);
 
-//        CoffeeViewAdaper adapter = new CoffeeViewAdaper(list);
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        //Adapter adapter = new Adapter();
+        recyclerView = findViewById(R.id.recyclerView);
+
+        adapter = new CoffeeViewAdapter(coffeeList);
+        recyclerView.setAdapter(adapter);
+
+        for (int i=0;i<6;i++){
+            coffeeList.add (new CoffeeData(coffee_img[i], coffee_name[i], coffee_price[i]));
+        }
+
+
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
         recyclerView.setLayoutManager(gridLayoutManager);
-//        recyclerView.setAdapter(adapter);
 
-
-//        mAdapter2 = new CoffeeViewAdaper(coffeeList);
-//        CoffeeViewAdaper.setAdapter(mAdapter2);
     }
 
-    public void additem(int image, String mainText, String subText,int point){
-//        CoffeeData item = new CoffeeData(image,mainText,subText,point);
-//
-//        item.setImage(image);
-//        item.setMainText(mainText);
-//        item.setSubText(subText);
-//
-//        coffeeList.add(item);
+    public void additem(int img, String name, int price){
+        CoffeeData item = new CoffeeData(img,name,price);
+
+        item.setImg(img);
+        item.setName(name);
+        item.setprice(price);
+
+        coffeeList.add(item);
     }
 }
